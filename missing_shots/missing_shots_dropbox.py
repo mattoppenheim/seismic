55197aa49834c45d5e8b6472855c0c1d8bcdb857
Matthew Oppenheim <moppenheim@sheartwatergeo.com>
2023-06-08 08:05:21 +0000

    missing_shots_dropbox now also works on a combined shot list from dropbox1 and dropbox2

--- missing_shots/missing_shots_dropbox.py
+++ missing_shots/missing_shots_dropbox.py
@@ -3,21 +3,22 @@
 Expects a sequence number to be supplied.
 To run on e.g. sequence 38 type:
 ./missing_shots.py 38
-Last update: 2023-06-04 Matthew Oppenheim.
-
+Dependancy:
+    range_strings.py needs to be in the same directory as this script
+Last update: 2023-06-08 Matthew Oppenheim.
 '''
 
 import argparse
+import logging
 import os
 from pathlib import Path
-import logging
-from range_strings import *
+from range_strings import find_duplicates, find_missing, get_ranges
 import sys
 
-logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 # default sequence value used if one is not supplied
-SEQ = '40'
+SEQ = '47'
 
 # base directory for folders containing shots
 DROPBOX1_DIR = r'/nfs/awa-data01/dropbox1/dropobp/'
@@ -26,107 +27,146 @@ DROPBOX1_DIR = r'/nfs/awa-data01/dropbox1/dropobp/'
 DROPBOX2_DIR = r'/nfs/awa-data02/dropbox2/dropobp/'
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('sequence', type=str, default=SEQ, help='sequence to find missing shots')
+
+class MissingShots():
 
 
-def display_duplicates(shots):
-    ''' Display information about duplicated shots. '''
-    duplicates = find_duplicates(shots)
-    if len(duplicates) == 0:
-        logging.info('no duplicates found')
-    else:
-        logging.info('duplicates: {}'.format(find_duplicates(shots)))
+    def __init__(self, directory_path,  *args):
+        self.main(directory_path, *args)
 
 
-def display_missing(missing):
-    ''' Display missing shot information. '''
-    if len(missing) == 0:
-        logging.info('no missing shots')
-        return
-    logging.info('missing shots: {}'.format(get_ranges(missing)))
-    logging.info('first missing shot: {}'.format(missing[0]))
-    logging.info('last missing shot: {}'.format(missing[-1]))
-    logging.info('number missing shots: {}'.format(len(missing)))
+    def display_duplicates(self, shots):
+        ''' Display information about duplicated shots. '''
+        duplicates = find_duplicates(shots)
+        if len(duplicates) == 0:
+            logging.info('no duplicates found')
+        else:
+            logging.info('duplicates: {}'.format(find_duplicates(shots)))
 
 
-def dropbox_dir_path(sequence, dropbox_dir):
-    dropbox_dir_path = os.path.join(dropbox_dir, sequence)
-    if not os.path.exists(dropbox_dir_path):
-        exit_code('cannot find directory: {}'.format(dropbox_dir_path))
-    return os.path.join(dropbox_dir, sequence)
+    def display_missing(self, missing):
+        ''' Display missing shot information. '''
+        if len(missing) == 0:
+            logging.info('no missing shots')
+            return
+        logging.info('missing shots: {}'.format(get_ranges(missing)))
+        logging.info('first missing shot: {}'.format(missing[0]))
+        logging.info('last missing shot: {}'.format(missing[-1]))
+        logging.info('number missing shots: {}'.format(len(missing)))
 
 
-def exit_code(message):
-    ''' Exits. '''
-    logging.info(message)
-    logging.info('exiting')
-    raise SystemExit
+
+    def display_shot_info(self, shots):
+        ''' Display information about the shots. '''
+        logging.info('\nfirst shot: {}'.format(shots[0]))
+        logging.info('last shot: {}'.format(shots[-1]))
+        missing = find_missing(shots)
+        self.display_missing(missing)
+        number_expected = abs(shots[-1] - shots[0]) + 1
+        logging.info('number expected files: {}'.format(number_expected))
+        self.display_duplicates(shots)
 
 
-def get_filenames(input_dir):
-    ''' Create a list of filenames in input_dir sorted in time order. '''
-    basepath = Path(input_dir)
-    try:
-        os.chdir(basepath)
-    except FileNotFoundError as e:
-        exit_code('The directory path is not found: {}'.format(basepath))
-    filenames = list(filter(os.path.isfile, os.listdir(basepath)))
-    filenames.sort(key=lambda x: os.path.getmtime(x))
-    return filenames
+    def drop_dir_path(self, sequence, dropbox_dir):
+        drop_dir_path = os.path.join(dropbox_dir, sequence)
+        if not os.path.exists(drop_dir_path):
+            self.exit_code('cannot find directory: {}'.format(drop_dir_path))
+        return os.path.join(dropbox_dir, sequence)
 
 
-def parse_arguments(*args):
-    ''' Parse command line arguments. '''
-    try:
-        args = ''.join(*args)
-    except TypeError:
-        logging.info('no arguments passed, using defaults:')
+    def exit_code(self, message):
+        ''' Exits. '''
+        logging.info(message)
+        logging.info('exiting')
+        raise SystemExit
 
 
-def shot_list(filename_list):
-    ''' Extract shots from the filenames. '''
-    shot_list = []
-    for filename in filename_list:
-        shot_list.append(int(filename[:5]))
-    # shot_list.sort()
-    return shot_list
+    def get_filenames(self, input_dir):
+        ''' Create a list of filenames in input_dir sorted in time order. '''
+        basepath = Path(input_dir)
+        try:
+            os.chdir(basepath)
+        except FileNotFoundError as e:
+            self.exit_code('The directory path is not found: {}'.format(basepath))
+        filenames = list(filter(os.path.isfile, os.listdir(basepath)))
+        filenames.sort(key=lambda x: os.path.getmtime(x))
+        return filenames
 
 
-def sort_shots(shot_list):
-    ''' Sort the shots. '''
-    # sometimes the shots are saved out of time order
-    if shot_list[0] > shot_list[-1]:
-        shot_list.sort(reverse=True)
-    else:
-        shot_list.sort()
-    logging.debug('shots_list: {}'.format(shot_list))
-    return shot_list
+    def is_inc(self, shot_list):
+        ''' Detect if shot_list is incrementing or decrementing. '''
+        if shot_list[0] > shot_list[-1]:
+            return False
+        return True
 
 
-def main(directory_path, args):
-    sequence = args.sequence.__str__()
-    logging.info('\nseq {}'.format(sequence))
-    directory_path = dropbox_dir_path(sequence, directory_path)
-    logging.info('directory: {}'.format(directory_path))
-    files = get_filenames(directory_path)
-    shots = shot_list(files)
-    shots = sort_shots(shots)
-    logging.info('first shot: {}'.format(shots[0]))
-    logging.info('last shot: {}'.format(shots[-1]))
-    missing = find_missing(shots)
-    display_missing(missing)
-    number_expected = abs(shots[-1] - shots[0]) + 1
-    logging.info('number expected files: {}'.format(number_expected))
-    display_duplicates(shots)
+    def parse_arguments(self, *args):
+        ''' Parse command line arguments. '''
+        try:
+            args = ''.join(*args)
+        except TypeError:
+            logging.info('no arguments passed, using defaults:')
+
+
+    def shot_list(self, filename_list):
+        ''' Extract shots from the filenames. '''
+        shot_list = []
+        for filename in filename_list:
+            shot_list.append(int(filename[:5]))
+        return shot_list
+
+
+    def sort_shots(self, shot_list):
+        ''' Sort the shots. '''
+        # sometimes the shots are saved out of time order
+        inc = self.is_inc(shot_list)
+        if inc:
+            shot_list.sort()
+        else:
+            shot_list.sort(reverse=True)
+        return shot_list
+
+
+    def main(self, directory_path, args):
+        sequence = args.sequence.__str__()
+        logging.info('\nseq {}'.format(sequence))
+        directory_path = self.drop_dir_path(sequence, directory_path)
+        logging.info('directory: {}'.format(directory_path))
+        files = self.get_filenames(directory_path)
+        self.shots = self.shot_list(files)
+        self.incrementing = self.is_inc(self.shots)
+        self.sorted_shots = self.sort_shots(self.shots)
+        self.display_shot_info(self.sorted_shots)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sequence', type=str, default=SEQ, help='sequence to find missing shots')
     # comment out the following line for testing
     args = parser.parse_args()
     # uncomment the line below for testing
-    # args = argparse.Namespace(sequence=41)
-    main(DROPBOX1_DIR, args)
-    main(DROPBOX2_DIR, args)
-
+    # args = argparse.Namespace(sequence=SEQ)
+    dropped1 = MissingShots(DROPBOX1_DIR, args)
+    dropped2 = MissingShots(DROPBOX2_DIR, args)
+    drop1_incrementing = dropped1.incrementing
+    drop2_incrementing = dropped2.incrementing
+    drop1_shots = dropped1.shots
+    drop2_shots = dropped1.shots
+    # I don't know why drop1_shots and drop2_shots are always returned sorted
+    # in ascending order, not in the order they exist in the class
+    if drop1_incrementing:
+        drop1_shots.sort()
+    else:
+        drop1_shots.sort(reverse=True)
+    if drop2_incrementing:
+        drop2_shots.sort()
+    else:
+        drop2_shots.sort(reverse=True)
+    drop2_shots = dropped2.sorted_shots
+    all_shots = [*drop1_shots, *drop2_shots]
+    sorted_all_shots = dropped1.sort_shots(all_shots)
+    logging.info('\nall_shots first: {} last: {}\n'.format(all_shots[0], all_shots[-1]))
+    logging.info('\nCombined shots for dropbox1 and dropbox2')
+    logging.info('\nranges for sorted_all_shots: {}'.format(get_ranges(all_shots)))
+    dropped1.display_shot_info(sorted_all_shots)
 
